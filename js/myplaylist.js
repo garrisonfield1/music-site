b9065b49fd9a5f36a156fb373d0bda41e11c9e98
Sean McintyreII <sean_mcintyre@hotmail.com>
2018-03-14 18:49:25 -0400

    site backup

---
+++ js/myplaylist.js
@@ -0,0 +1,52 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+	    {
+        mp3:'mix/sept.mp3',
+        oga:'mix/sept.ogg',
+        title:'Sept 22 / Live @ Smiths',
+        artist:'Nathan Angelo',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'4:30',
+        cover:'mix/nathan3.jpg'
+		
+		
+    },
+    
+    {
+        mp3:'mix/dont.mp3',
+        oga:'mix/dont.ogg',
+        title:'Dont leave here tonight',
+        artist:'Sam Thacker',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'4:30',
+        cover:'mix/sam.jpg'
+		
+		
+    },
+	    {
+        mp3:'mix/union.mp3',
+        oga:'mix/union.ogg',
+        title:'Union',
+        artist:'John Hopkins',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'4:30',
+        cover:'mix/bs.jpg'
+		
+		
+    }
+
+];
